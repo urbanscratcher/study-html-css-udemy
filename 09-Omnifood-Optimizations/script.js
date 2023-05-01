a134dd2add2602733eba4991243c71c1c032d1d7
Joun <urbanscratcher@gmail.com>
2023-05-02 00:41:12 +0900

    done

--- 09-Omnifood-Optimizations/script.js
+++ 09-Omnifood-Optimizations/script.js
@@ -1,3 +1,65 @@
+// Set Current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const headerEl = document.querySelector(".header");
+const btnEl = document.querySelector(".btn-mobile-nav");
+
+btnEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+console.log(allLinks);
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to the top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const bodyEl = document.querySelector("body");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    !ent.isIntersecting
+      ? bodyEl.classList.add("sticky")
+      : bodyEl.classList.remove("sticky");
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
